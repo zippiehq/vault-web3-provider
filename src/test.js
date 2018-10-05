dbc6321e6b49521a29f1066af48b5108a9458dc3
Lewis Rockliffe <r0kk3rz@gmail.com>
2018-10-05 20:04:47 +0200

    update tests

--- src/test.js
+++ src/test.js
@@ -1,10 +1,11 @@
-
 var vault = require('@zippie/vault-api')
 var vaultSecp256k1 = require('@zippie/vault-api/src/secp256k1.js')
 var zippieprovider = require('./index.js')
 var Web3 = require('web3');
 
-vault.init().then((result) => {
+var opts = {vaultURL: 'https://vault.dev.zippie.org'}
+
+vault.init(opts).then((result) => {
   console.log('got inited:')
   console.log(result)
   var provider = zippieprovider.init(vault, vaultSecp256k1, { network: 'kovan' })
@@ -14,21 +15,15 @@ vault.init().then((result) => {
     web3.eth.getAccounts().then((accounts) => {
       console.log('accounts:')
       console.log(accounts)
-      web3.eth.sendTransaction({from: accounts[0],
-        gasPrice: "2000000000",
-        gas: "21000",
-        to: '0x21ef24ffb2116f44e7918a80cea4f52a2ea72b17',
-        value: "1",
-        data: ""}).once('transactionHash', function(hash){
-          console.log('transaction ' + hash)
-        })
-        .once('receipt', function(receipt){
-          console.log('receipt ' + receipt)
-        })
-        .on('confirmation', function(confirmationNumber, receipt) {
-          console.log('confirmation ' + confirmationNumber + " " + receipt)
-        })
-        .on('error', console.error)
+
+      web3.eth.getBlockNumber().then((bnl) => {
+	 console.log('block number: ' + bnl)
+         web3.eth.getBlock(bnl).then((blockinfo) => {
+	  console.log('block info: ')
+          console.log(blockinfo)
+         })
+      })
+      web3.eth.getBalance(accounts[0]).then((balance) => console.log('balance: ' + balance))
     })
   })
   
